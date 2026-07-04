972685c6138107ea070622ef6d9ed8340bd5f4eb
saribahamed9-gif <saribahamed9@gmail.com>
2026-07-05 03:03:04 +0600

    Add products data to products.js

---
+++ data/products.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    id: 1,
+    name: "Pearl Earrings",
+    price: 450,
+    image: "images/p1.jpg"
+  },
+  {
+    id: 2,
+    name: "Gold Earrings",
+    price: 550,
+    image: "images/p2.jpg"
+  },
+  {
+    id: 3,
+    name: "Crystal Earrings",
+    price: 650,
+    image: "images/p3.jpg"
+  }
+];
